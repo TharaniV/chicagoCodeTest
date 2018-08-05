dd8543f064db8c8924dbaa4955083c8043e24a9f
Tharani Venkatanarayanan <thara.venkatanarayanan@gmail.com>
2018-08-04 21:23:02 -0700

    Changed path

--- config/common.js
+++ config/common.js
@@ -1,4 +1,4 @@
 module.exports = {
-  file_path: process.env.FILE_PATH || '../chicagoCodeTest/data/testcsv.csv',
+  file_path: process.env.FILE_PATH || '../data/testcsv.csv',
   port: process.env.PORT || 3000
 };
